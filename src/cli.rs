9ea5cce15d07174cc6eaaf91c7d28b5427e3752d
Matt Gleich <git@mattglei.ch>
2021-08-08 22:04:18 -0400

    feat: register new cmd
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,52 @@
 use std::path::Path;
 
-use clap::{App, AppSettings, ArgMatches, SubCommand};
+use anyhow::Result;
+use clap::{App, AppSettings, Arg, ArgMatches};
 
+use crate::cmds::new;
 use crate::conf;
 
-pub fn setup<'a>() -> ArgMatches<'a> {
+pub fn setup() -> Result<ArgMatches> {
+	// Getting a list of the classes
+	let config = conf::read(true)?;
+	let classes: Vec<String> = config.classes.into_iter().map(|c| c.name).collect();
+
 	let mut app = App::new("kiwi")
 		.version("v1.0.0")
 		.author("Matt Gleich <email@mattglei.ch>")
 		.about("🥝 Schoolwork as code")
-		.setting(AppSettings::ArgRequiredElseHelp);
+		.setting(AppSettings::ArgRequiredElseHelp)
+		.subcommand(
+			App::new("new")
+				.about("Create a new document")
+				.arg(
+					Arg::new("name")
+						.long("name")
+						.short('n')
+						.about("Name of the file")
+						.takes_value(true),
+				)
+				.arg(
+					Arg::new("class")
+						.long("class")
+						.short('c')
+						.about("Name of the class")
+						.takes_value(true)
+						.possible_values(
+							&classes.iter().map(|s| s.as_ref()).collect::<Vec<&str>>(),
+						),
+				)
+				.arg(
+					Arg::new("type")
+						.long("type")
+						.short('t')
+						.about("Document type")
+						.takes_value(true)
+						.possible_values(&new::TYPES),
+				),
+		);
 	if !Path::new(conf::FNAME).exists() {
-		app = app.subcommand(SubCommand::with_name("setup").about("Setup a kiwi project"));
+		app = app.subcommand(App::new("setup").about("Setup a kiwi project"));
 	}
-	app.get_matches()
+	Ok(app.get_matches())
 }
