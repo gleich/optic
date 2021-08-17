be3ca0c3e09e16edd8d916b35820cd7c8b2fcddb
Matt Gleich <git@mattglei.ch>
2021-08-17 00:33:26 -0400

    refactor: use Steps struct in new cmd
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/cmds/new.rs
+++ src/cmds/new.rs
@@ -9,8 +9,19 @@ use dialoguer::theme::Theme;
 use crate::conf::{Config, DocType, Format};
 use crate::{cli, conf, inject};
 
-#[derive(Debug)]
-struct File {
+pub fn run(matches: &ArgMatches, prompt_theme: &dyn Theme) {
+	let mut steps = Steps {
+		branch: None,
+		config: conf::read(false).expect("Failed to read from configuration file"),
+		matches: matches.subcommand_matches("new").unwrap(),
+	};
+	steps
+		.ask(prompt_theme)
+		.expect("Failed to ask user for info about the branch");
+	steps.create().expect("Failed to create the file");
+}
+
+struct Branch {
 	name: String,
 	class: String,
 	doc_type: DocType,
@@ -19,73 +30,75 @@ struct File {
 	root_template_path: PathBuf,
 }
 
-pub fn run(matches: &ArgMatches, prompt_theme: &dyn Theme) {
-	let subcommand_matches = matches.subcommand_matches("new").unwrap();
-	let config = conf::read(false).expect("Failed to read from the configuration file");
-	let file = ask(&config, &subcommand_matches, prompt_theme)
-		.expect("Failed to ask user for info about the branch");
-	create(&file, &config).expect("Failed to create the file");
+struct Steps<'a> {
+	branch: Option<Branch>,
+	config: Config,
+	matches: &'a ArgMatches,
 }
 
-/// Ask the user information
-fn ask(config: &Config, matches: &ArgMatches, prompt_theme: &dyn Theme) -> Result<File> {
-	let format = conf::Format::from_str(&cli::flag_or_ask_select(
-		matches,
-		prompt_theme,
-		"format",
-		"Format",
-		conf::Format::to_vec(),
-	)?)?;
+impl Steps<'_> {
+	/// Ask the user information
+	pub fn ask(&mut self, prompt_theme: &dyn Theme) -> Result<()> {
+		let format = conf::Format::from_str(&cli::flag_or_ask_select(
+			&self.matches,
+			prompt_theme,
+			"format",
+			"Format",
+			conf::Format::to_vec(),
+		)?)?;
 
-	Ok(File {
-		name: cli::flag_or_ask_input(matches, prompt_theme, "name", "Name")?,
-		class: cli::flag_or_ask_select(
-			matches,
-			prompt_theme,
-			"class",
-			"Class",
-			config.classes.iter().map(|c| c.name.clone()).collect(),
-		)?,
-		doc_type: conf::DocType::from_str(&cli::flag_or_ask_select(
-			matches,
-			prompt_theme,
-			"type",
-			"Type",
-			conf::DocType::to_vec(),
-		)?)
-		.unwrap(),
-		branch_template_path: Path::new(&cli::flag_or_ask_select(
-			matches,
-			prompt_theme,
-			"branch",
-			"Branch template",
-			conf::list_templates(&format, &conf::TemplateType::Branch)?,
-		)?)
-		.to_path_buf(),
-		root_template_path: Path::new(&cli::flag_or_ask_select(
-			matches,
-			prompt_theme,
-			"root",
-			"Root template",
-			conf::list_templates(&format, &conf::TemplateType::Root)?,
-		)?)
-		.to_path_buf(),
-		format,
-	})
-}
-
-fn create(file: &File, config: &Config) -> Result<()> {
-	// Create file contents
-	let path = Path::new("templates")
-		.join(conf::TemplateType::Branch.to_string())
-		.join(&file.branch_template_path);
-	let contents = inject::BaseData {
-		doc_name: &file.name,
-		class_name: &file.class,
-		format: &file.format,
-		config,
+		self.branch = Some(Branch {
+			name: cli::flag_or_ask_input(&self.matches, prompt_theme, "name", "Name")?,
+			class: cli::flag_or_ask_select(
+				&self.matches,
+				prompt_theme,
+				"class",
+				"Class",
+				self.config.classes.iter().map(|c| c.name.clone()).collect(),
+			)?,
+			doc_type: conf::DocType::from_str(&cli::flag_or_ask_select(
+				&self.matches,
+				prompt_theme,
+				"type",
+				"Type",
+				conf::DocType::to_vec(),
+			)?)
+			.unwrap(),
+			branch_template_path: Path::new(&cli::flag_or_ask_select(
+				&self.matches,
+				prompt_theme,
+				"branch",
+				"Branch template",
+				conf::list_templates(&format, &conf::TemplateType::Branch)?,
+			)?)
+			.to_path_buf(),
+			root_template_path: Path::new(&cli::flag_or_ask_select(
+				&self.matches,
+				prompt_theme,
+				"root",
+				"Root template",
+				conf::list_templates(&format, &conf::TemplateType::Root)?,
+			)?)
+			.to_path_buf(),
+			format,
+		});
+		Ok(())
 	}
-	.inject_into(&fs::read_to_string(path)?)?;
-	println!("{}", contents);
-	Ok(())
+
+	pub fn create(&self) -> Result<()> {
+		// Create file contents
+		let branch = self.branch.as_ref().unwrap();
+		let path = Path::new("templates")
+			.join(conf::TemplateType::Branch.to_string())
+			.join(&branch.branch_template_path);
+		let contents = inject::BaseData {
+			doc_name: &branch.name,
+			class_name: &branch.class,
+			format: &branch.format,
+			config: &self.config,
+		}
+		.inject_into(&fs::read_to_string(path)?)?;
+		println!("{}", contents);
+		Ok(())
+	}
 }
