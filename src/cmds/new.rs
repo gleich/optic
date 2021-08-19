e0abc9afc7234e9f310da455910f971979e5bb5e
Matt Gleich <git@mattglei.ch>
2021-08-18 21:28:02 -0400

    refactor: switch new cmd back to regular function calls
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/cmds/new.rs
+++ src/cmds/new.rs
@@ -14,15 +14,11 @@ use crate::out::success;
 use crate::{cli, conf, inject};
 
 pub fn run(matches: &ArgMatches, prompt_theme: &dyn Theme) {
-	let mut steps = Steps {
-		branch: None,
-		config: conf::read(false).expect("Failed to read from configuration file"),
-		matches: matches.subcommand_matches("new").unwrap(),
-	};
-	steps
-		.ask(prompt_theme)
-		.expect("Failed to ask user for info about the branch");
-	steps.create().expect("Failed to create the file");
+	let config = conf::read(false).expect("Failed to read from configuration file");
+	let matches = matches.subcommand_matches("new").unwrap();
+	let branch =
+		ask(matches, &config, prompt_theme).expect("Failed to ask user for info about the branch");
+	create(&branch, &config).expect("Failed to create the file");
 }
 
 struct Branch {
@@ -34,106 +30,93 @@ struct Branch {
 	root_template_path: PathBuf,
 }
 
-struct Steps<'a> {
-	branch: Option<Branch>,
-	config: Config,
-	matches: &'a ArgMatches,
+/// Ask the user information
+fn ask(matches: &ArgMatches, config: &Config, prompt_theme: &dyn Theme) -> Result<Branch> {
+	let format = conf::Format::from_str(&cli::flag_or_select(
+		matches,
+		prompt_theme,
+		"format",
+		"Format",
+		Format::VARIANTS.to_vec(),
+	)?)?;
+
+	let branch = Branch {
+		name: cli::flag_or_input(matches, prompt_theme, "name", "Name")?,
+		class: cli::flag_or_select(
+			matches,
+			prompt_theme,
+			"class",
+			"Class",
+			config.classes.iter().map(|c| c.name.as_str()).collect(),
+		)?,
+		doc_type: conf::DocType::from_str(&cli::flag_or_select(
+			matches,
+			prompt_theme,
+			"type",
+			"Type",
+			DocType::VARIANTS.to_vec(),
+		)?)
+		.unwrap(),
+		branch_template_path: Path::new(&cli::flag_or_select(
+			matches,
+			prompt_theme,
+			"branch",
+			"Branch template",
+			conf::list_templates(&format, &conf::TemplateType::Branch)?
+				.iter()
+				.map(AsRef::as_ref)
+				.collect(),
+		)?)
+		.to_path_buf(),
+		root_template_path: Path::new(&cli::flag_or_select(
+			matches,
+			prompt_theme,
+			"root",
+			"Root template",
+			conf::list_templates(&Format::LaTeX, &conf::TemplateType::Root)?
+				.iter()
+				.map(AsRef::as_ref)
+				.collect(),
+		)?)
+		.to_path_buf(),
+		format,
+	};
+	println!();
+	success("Obtained information about file");
+	Ok(branch)
 }
 
-impl Steps<'_> {
-	/// Ask the user information
-	pub fn ask(&mut self, prompt_theme: &dyn Theme) -> Result<()> {
-		let format = conf::Format::from_str(&cli::flag_or_select(
-			&self.matches,
-			prompt_theme,
-			"format",
-			"Format",
-			Format::VARIANTS.to_vec(),
-		)?)?;
-
-		self.branch = Some(Branch {
-			name: cli::flag_or_input(&self.matches, prompt_theme, "name", "Name")?,
-			class: cli::flag_or_select(
-				&self.matches,
-				prompt_theme,
-				"class",
-				"Class",
-				self.config
-					.classes
-					.iter()
-					.map(|c| c.name.as_str())
-					.collect(),
-			)?,
-			doc_type: conf::DocType::from_str(&cli::flag_or_select(
-				&self.matches,
-				prompt_theme,
-				"type",
-				"Type",
-				DocType::VARIANTS.to_vec(),
-			)?)
-			.unwrap(),
-			branch_template_path: Path::new(&cli::flag_or_select(
-				&self.matches,
-				prompt_theme,
-				"branch",
-				"Branch template",
-				conf::list_templates(&format, &conf::TemplateType::Branch)?
-					.iter()
-					.map(AsRef::as_ref)
-					.collect(),
-			)?)
-			.to_path_buf(),
-			root_template_path: Path::new(&cli::flag_or_select(
-				&self.matches,
-				prompt_theme,
-				"root",
-				"Root template",
-				conf::list_templates(&Format::LaTeX, &conf::TemplateType::Root)?
-					.iter()
-					.map(AsRef::as_ref)
-					.collect(),
-			)?)
-			.to_path_buf(),
-			format,
-		});
-		println!();
-		success("Obtained information about file");
-		Ok(())
+fn create(branch: &Branch, config: &Config) -> Result<()> {
+	let content = inject::inject(
+		branch.doc_type.to_string(),
+		branch.root_template_path.to_str().unwrap(),
+		branch.class.to_string(),
+		&branch.format,
+		config,
+		fs::read_to_string(
+			Path::new("templates")
+				.join(conf::TemplateType::Branch.to_string())
+				.join(&branch.branch_template_path),
+		)?,
+	)?;
+	success("Injected variables into template");
+	let path = Path::new("docs")
+		.join(&branch.class)
+		.join(Month::from_u32(Local::now().month()).unwrap().name())
+		.join(&branch.doc_type.to_string())
+		.join(format!(
+			"{}.{}",
+			&branch.name,
+			match branch.format {
+				Format::LaTeX => "tex",
+				Format::Markdown => "md",
+			}
+		));
+	fs::create_dir_all(path.parent().unwrap())?;
+	if path.exists() {
+		bail!("{} already exists", &path.to_str().unwrap());
 	}
-
-	pub fn create(&self) -> Result<()> {
-		let branch = self.branch.as_ref().unwrap();
-		let content = inject::inject(
-			branch.doc_type.to_string(),
-			branch.root_template_path.to_str().unwrap(),
-			branch.class.to_string(),
-			&branch.format,
-			&self.config,
-			fs::read_to_string(
-				Path::new("templates")
-					.join(conf::TemplateType::Branch.to_string())
-					.join(&branch.branch_template_path),
-			)?,
-		)?;
-		success("Injected variables into template");
-		let path = Path::new("docs")
-			.join(&branch.class)
-			.join(Month::from_u32(Local::now().month()).unwrap().name())
-			.join(&branch.doc_type.to_string())
-			.join(format!(
-				"{}.{}",
-				&branch.name,
-				match branch.format {
-					Format::LaTeX => "tex",
-					Format::Markdown => "md",
-				}
-			));
-		fs::create_dir_all(path.parent().unwrap())?;
-		if path.exists() {
-			bail!("{} already exists", &path.to_str().unwrap());
-		}
-		fs::write(&path, content)?;
-		success(&format!("Created {}", &path.to_str().unwrap()));
-		Ok(())
-	}
+	fs::write(&path, content)?;
+	success(&format!("Created {}", &path.to_str().unwrap()));
+	Ok(())
 }
