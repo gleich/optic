9bed49e7b384fc80d20e00a193e93d300edfccaa
Matt Gleich <git@mattglei.ch>
2021-08-19 22:26:54 -0400

    fix: replace &amp; to &
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/cmds/build.rs
+++ src/cmds/build.rs
@@ -111,7 +111,12 @@ fn extract_branch_data(content: &str, branch_path: &PathBuf) -> Result<Branch> {
 			let trimmed_line = line.trim();
 			let prefix = format!("{} {} ", name, ARROW_CHARACTERS);
 			if trimmed_line.starts_with(&prefix) {
-				return Some(trimmed_line.trim_start_matches(&prefix).to_string());
+				return Some(
+					trimmed_line
+						.trim_start_matches(&prefix)
+						.to_string()
+						.replace("&amp;", "&"),
+				);
 			}
 		}
 		None
