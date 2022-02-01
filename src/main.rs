4e86f21fd8bbe25c561f1accaf779db6c1a1806f
Matt Gleich <git@mattglei.ch>
2022-02-01 01:20:59 -0500

    feat: reset
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,3 @@
-use cmds::{build, check, commit, new, setup};
-use out::custom_dialoguer_theme;
-
-mod branches;
-mod cli;
-mod cmds;
-mod conf;
-mod inject;
-mod out;
-
 fn main() {
-	let prompt_theme = custom_dialoguer_theme();
-	let matches = cli::setup().expect("Failed to setup CLI");
-	if matches.is_present("setup") {
-		setup::run(&prompt_theme);
-	} else if matches.is_present("new") {
-		new::run(matches.subcommand_matches("new").unwrap(), &prompt_theme);
-	} else if matches.is_present("build") {
-		build::run(matches.subcommand_matches("build").unwrap());
-	} else if matches.is_present("check") {
-		check::run(&prompt_theme);
-	} else if matches.is_present("commit") {
-		commit::run();
-	}
+	println!("Hello World");
 }
